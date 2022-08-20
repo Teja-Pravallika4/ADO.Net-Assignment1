8885e8bbe4ee158bad535be0d954684e93468b2f
DELLPC1 <DELLPC1@DESKTOP-AA9KGR9>
2022-08-20 17:08:41 +0530

    Add project files.

---
+++ BootStrapFootBall/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootStrapFootBall.Models;
+
+namespace BootStrapFootBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            List<MstFootball> qry = new List<MstFootball>();
+            using (FootBallEntities dc = new FootBallEntities())
+            {
+                qry = (from s in dc.FootBallLeagues
+                       where s.Status == "Win"
+
+                       select new MstFootball
+                       {
+                           MatchID = s.MatchID,
+                           TeamName1 = s.TeamName1,
+                           TeamName2 = s.TeamName2,
+                           Status = s.Status,
+                           WinningTeam = s.WinningTeam,
+                           Points = s.Points,
+
+
+                       }).ToList();
+
+            }
+            return View(qry);
+        }
+    }
+}
+
+       
